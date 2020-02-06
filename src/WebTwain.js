0e976034c48174de4fb9121f7f456e6492c7fe50
Daniel F Yepes O <danielfyo@hotmail.com>
2020-02-06 14:13:16 -0500

    ajustes operación registro licencia DWT

--- src/WebTwain.js
+++ src/WebTwain.js
@@ -14,7 +14,7 @@ export class WebTwain extends Component {
      *    resource files that you obtain after purchasing a key
      */
     Dynamsoft.WebTwainEnv.Trial = true;
-    Dynamsoft.WebTwainEnv.ProductKey = "t0141cQMAAJxnh4Yp1hYz6iW67PSArOVbdY/p5u2Vmw8F73QqYY1s84+ZUw30+OUWFnwz5ukkpI5e7UxA2Gg0ctzT/bL3IZEco29mOYT0ZMJk6khg1HtrPoz9MYv8zCVOnWnEhWKjCWOxYdiPwpu9Iu75NpowFhuG/SR6PpOukYTRhLHYEO/G2p+CFe5+AKpw";
+    Dynamsoft.WebTwainEnv.ProductKey = 't0141cQMAAJxnh4Yp1hYz6iW67PSArOVbdY/p5u2Vmw8F73QqYY1s84+ZUw30+OUWFnwz5ukkpI5e7UxA2Gg0ctzT/bL3IZEco29mOYT0ZMJk6khg1HtrPoz9MYv8zCVOnWnEhWKjCWOxYdiPwpu9Iu75NpowFhuG/SR6PpOukYTRhLHYEO/G2p+CFe5+AKpw';
     Dynamsoft.WebTwainEnv.ResourcesPath = "https://tst.dynamsoft.com/libs/dwt/15.0";
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
@@ -22,9 +22,6 @@ export class WebTwain extends Component {
 
   constructor(props) {
     super(props);
-  }
-
-  loadDWT() {
     Dynamsoft.WebTwainEnv.Load();
   }
 
@@ -39,15 +36,14 @@ export class WebTwain extends Component {
         dwObject.AcquireImage({}, onAcquireImageSuccess, onAcquireImageFailure);
       }
     } else {
-      alert("Please press 'Load DWT' first!");
+      alert('El componente DWT no ha sido cargado');
     }
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.loadDWT}>Load DWT</button><span>   </span>
-        <button onClick={this.handleClick}>Scan Document</button>
+        <button onClick={this.handleClick}>Escanear documento</button>
         <div id="dwtcontrolContainer"></div>
       </div>
     );
